ab95a31080d105ffb8f37addbd250f07058325e6
Ewerton  Guimarães <ewertonpg@hotmail.com>
2021-08-30 22:01:18 -0300

    Inclusão de controller de Doador
    Criado controller e página para cadastro de doador

---
+++ Controllers/DoadorController.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteFatec.Controllers
+{
+    public class DoadorController : Controller
+    {
+        // GET: DoadorController
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+
+        // GET: DoadorController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DoadorController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DoadorController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DoadorController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DoadorController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DoadorController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DoadorController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Doador.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SiteFatec.Models
+{
+    public class Doador
+    {
+        public string nome;
+        public string sobrenome;
+        public string endereco;
+        public string cidade;
+        public string estado;
+        public string dataNasc;
+        public string email;
+        public int telefone;
+    }
+}
