6a3c488f73fb5d49bd58f533fe369c3f8538570c
Ewerton  Guimarães <ewertonpg@hotmail.com>
2021-09-14 16:40:30 -0300

    adição de extensoes e conexão com banco de dados
    Adicionado Dapper, ORacleConnection e classe para conectar ao banco de dados

---
+++ DAO/DoadorDao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Dapper;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SiteFatec.Models;
+
+namespace SiteFatec.DAO
+{
+    public class DoadorDao : DaoConexao
+    {
+        public DoadorDao(IDbConnection dbConnection) : base(dbConnection)
+        {
+        }
+        public IEnumerable<string> GetDoadores()
+        {
+            try
+            {
+                string SQL = @"SELECT * FROM T2S.RASTREAMENTO_CORREIOS";
+                var result = DbConnection.Query<string>(SQL);
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -18,6 +18,8 @@ namespace SiteFatec
             Configuration = configuration;
         }
 
+        
+
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
