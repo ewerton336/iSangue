025c5826ee29120f59a8c0421ebf467960898ff9
Ewerton  Guimarães <ewertonpg@hotmail.com>
2021-10-21 17:23:02 -0300

    inclusão da dao calendarioEvento

---
+++ DAO/CalendarioEventoDao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using iSangue.Models;
+using MySqlConnector;
+
+namespace iSangue.DAO
+{
+    public class CalendarioEventoDao : DaoConexao
+    {
+
+        public CalendarioEventoDao(MySqlConnection dbConnection) : base(dbConnection)
+        {
+
+        }
+
+
+        public async Task<IEnumerable<CalendarioEvento>> GetCalendariosEventos()
+        {
+
+            try
+            {
+                string SQL = @"SELECT
+                                ID
+                                , nm_evento domeEvento
+                                , dt_evento dataEvento
+                                ,qt_interessado quantidadeInteressados
+                                ,cd_entidade_coletora_fk entidadeColetoraID
+                                ,cd_cedente_local_fk cedenteLocalID";
+                var result = await DbConnection.QueryAsync<CalendarioEvento>(SQL);
+                DbConnection.Close();
+                return result;
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+
+
+        }
+
+    }
+
+}
--- Models/CalendarioEvento.cs
+++ Models/CalendarioEvento.cs
@@ -7,36 +7,27 @@ using System.Threading.Tasks;
 
 namespace iSangue.Models
 {
-    [Table ("calendario_evento")]
+
     public class CalendarioEvento
     {
         [Required, Key]
         public int id { get; set; }
 
-        [Column("nm_evento")]
         [Display(Name = "Nome do Evento")]
         [DataType(DataType.Text)]
         public string nomeEvento { get; set; }
 
-        [Column ("dt_evento")]
         [Display(Name = "Data do Evento")]
         [DataType(DataType.DateTime)]
         public DateTime dataEvento { get; set; }
 
-        [Column("qt_interessado")]
         [Display(Name = "Quantidade de interessados")]
         public int quantidadeInteressados { get; set; }
 
-        [Column("endereco_evento")]
-        [Display(Name = "Endereço do evento")]
-        public string enderecoLocalColeta { get; set; }
+        [Display(Name = "ID Entidade Coletora")]
+        public int entidadeColetoraID { get; set; }
 
-        [Column("num_endereco_evento")]
-        [Display(Name = "Numero do endereço")]
-        public int numeroEnderecoLocalColeta { get; set; }
-
-        [Column("entidade_coletora")]
-        [Display(Name = "Entidade Coletora")]
-        public string entidadeColetora { get; set; }
+        [Display(Name = "ID Cedente Local")]
+        public int cedenteLocalID { get; set; }
     }
 }
